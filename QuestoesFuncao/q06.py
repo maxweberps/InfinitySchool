4e4d37464dc90a33daf8767c644d8d21762e798e
maxweberps <127635922+maxweberps@users.noreply.github.com>
2023-07-27 19:28:28 -0300

    Delete QuestoesFuncao directory

--- QuestoesFuncao/q06.py
+++
@@ -1,9 +0,0 @@
-"""
-6. Escreva uma função que imprima uma sequencia de 0 a 100.
-"""
-
-def Contar():
-    for i in range(0,101):
-        print(i)
-
-Contar()
--- QuestoesFuncao/q08.py
+++
@@ -1,8 +0,0 @@
-"""
-8. Escreva uma função que leia o tempo de uma viagem e a velocidade média nessa viagem, em seguida informe o
-consumo total de combustivel da viagem, levando em consideração que o veiculo faz 10km/l
-"""
-
-def viagem():
-    tempo = int(input('Tempo da viagem (hora): '))
-    velocidade = float(input('Velocidade média (km/h): '))
