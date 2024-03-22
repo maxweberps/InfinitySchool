f15eacb5b432527c6bcf1fe387804b6435ece251
maxweberps <127635922+maxweberps@users.noreply.github.com>
2024-03-21 22:10:51 -0300

    Update script.js

--- 2 - Aulas Oficiais/519Aula07js/script.js
+++ 2 - Aulas Oficiais/519Aula07js/script.js
@@ -1,4 +1,4 @@
-// PROGRAMA 1
+// TAREFA 1
 /*function saudar(nome,horario){
     if(horario >= 5 && horario <= 12){
         return `Bom dia ${nome}`
@@ -14,7 +14,7 @@ let horario = Number(prompt('Horário: '))
 
 alert(saudar(nome,horario))*/
 
-// PROGRAMA 2
+// TAREFA 2
 /*let vogais = 'aeiou'
 
 function contar_vogais(palavra){
@@ -29,3 +29,65 @@ function contar_vogais(palavra){
 
 palavra = prompt('Digite uma palavra: ')
 alert(`A palavra ${palavra} possui ${contar_vogais(palavra)} vogais`)*/
+
+// DESAFIO
+const maiuscula = 'ABCDEFGHIJLKMNOPQRSTUVWXZ'
+const minuscula = 'abcdefghijklmnopqrstuvwxz'
+const numero = '0123456789'
+const especial = '@&*#/{}][$'
+
+function verificar_senha(senha){
+    let cont_req = 0
+    //requisito 1: ter 8  ou mais caracteres
+    if (senha.length >= 8){
+        cont_req++
+    }
+
+    //requisito 2: 1 MAIUSCULA
+    for(char of senha){
+        if (maiuscula.includes(char)){
+            cont_req++
+            break
+        }
+    }
+
+    //requisito 3: 1 minuscula
+    for (char of senha){
+        if (minuscula.includes(char)){
+            cont_req++
+            break
+        }
+    }
+
+    //requisito 4: 1 número
+    for (char of senha){
+        if (numero.includes(char)){
+            cont_req++
+            break
+        }
+    }
+
+    //requisito 5: 1 char especial
+    for (char of senha){
+        if (especial.includes(char)){
+            cont_req++
+            break
+        }
+    }
+
+    console.log(`Quantidade de requisito atendidos: ${cont_req}`)
+
+    if ( cont_req >= 1 && cont_req <= 2){
+        return 'fraca'
+    } else if (cont_req >= 3 && cont_req <= 4){
+        return 'média'
+    } else if (cont_req >= 5){
+        return 'forte'
+    } else{
+        return 'não atende nenhum requisito!'
+    }
+}
+    
+
+senha = prompt('Digite sua senha: ')
+alert(`Sua senha é ${verificar_senha(senha)}`)
