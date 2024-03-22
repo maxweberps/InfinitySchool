8ae24b5a12b49f18ed5d0b1816f3c1703f6c4d2c
maxweberps <127635922+maxweberps@users.noreply.github.com>
2024-03-21 21:23:56 -0300

    Add files via upload

---
+++ 2 - Aulas Oficiais/519Aula07js/script.js
@@ -0,0 +1,31 @@
+// PROGRAMA 1
+/*function saudar(nome,horario){
+    if(horario >= 5 && horario <= 12){
+        return `Bom dia ${nome}`
+    } else if (horario > 12 && horario <= 18){
+        return `Boa tarde ${nome}`
+    } else{
+        return `Boa noite ${nome}`
+    }
+}
+
+let nome = prompt('Seu nome: ')
+let horario = Number(prompt('Horário: '))
+
+alert(saudar(nome,horario))*/
+
+// PROGRAMA 2
+/*let vogais = 'aeiou'
+
+function contar_vogais(palavra){
+    cont_vogal = 0
+    for (letra of palavra.toLowerCase()){
+        if (vogais.includes(letra)){
+            cont_vogal ++
+        }
+    }
+    return cont_vogal
+}
+
+palavra = prompt('Digite uma palavra: ')
+alert(`A palavra ${palavra} possui ${contar_vogais(palavra)} vogais`)*/
