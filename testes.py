98c2fead424fe4d74a572cd7e0c6f33524aea737
maxweberps <max.ps0235@gmail.com>
2024-04-01 21:39:38 -0300

    upload

--- testes.py
+++ testes.py
@@ -1,3 +1,29 @@
-palavras = ['gato', 'cachorro', 'elefante', 'leão', 'tigre','papagaio']
+class Veiculo:
 
-print(list(filter(lambda x: len(x) > 5, palavras)))
+    def __init__(self, marca):
+        self.marca = marca
+
+    def acelerar(self):
+        print("Veículo acelerando...")
+
+
+class Carro(Veiculo):
+
+    def acelerar(self):
+        print("Carro acelerando...")
+
+
+class Moto(Veiculo):
+
+    def acelerar(self):
+        print("Moto acelerando...")
+
+
+def testar_aceleracao(veiculo):
+    veiculo.acelerar()
+
+
+veiculos = [Carro("Toyota"), Moto("Honda")]
+
+for veiculo in veiculos:
+    testar_aceleracao(veiculo)
